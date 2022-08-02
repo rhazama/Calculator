2b665d5e74ed50c60e3c675a7203fc744658c0b1
rhazama <rhazama01@gmail.com>
2022-08-01 17:55:35 -0700

    buttons function and c clears

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let display = document.getElementById("display");
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
